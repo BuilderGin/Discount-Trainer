e9929a0add3b8d973e9cc24e99dd0c8d512cbfe4
BuildTools <unconfigured@null.spigotmc.org>
2022-02-17 19:56:17 +0100

    Converted Game to Exe

---
+++ DiscountTrainerV0.9/main/assets/developer.py
@@ -0,0 +1,42 @@
+'''
+Created on Jan 26, 2022
+@summary: Developer change this for each edit
+@author: git:buildergin
+Update on Feb 04, 2022
+'''
+
+# #############################################
+# Define Default variable for the app
+# #############################################
+
+
+"""
+    Define some variables for the app
+    :param: Developer can change it if needed
+    :return: Use the variable defined below for futher use
+"""
+
+
+## APP TITLE
+APP_NAME= 'Sport for Discount - RockIt'
+
+
+## SPLASH SCREEN DATA
+COPYRIGHT_TEXT = '©2022 RockIT. ALLE RECHTEN VOORBEHOUDEN | DESIGN & BUILD BY GIN'
+
+
+## MENU SCREEN DATA
+CREATED = 'Created by: Team RockIT | Gin'
+GAME_NAME= 'Discount Trainer'
+
+
+## ABOUT SCREEN DATA
+CREATOR = 'GIN'
+UPDATE_DATE = '17 FEBERUARI 2021'
+GITHUB_LINK= 'https://github.com/BuilderGin/Discount-Trainer'
+
+
+## USED IN MENU & ABOUT SCREEN
+GAME_VERSION = '0.95'
+
+PRINT_TIME = "Discount Will Be Printed Soon..."
